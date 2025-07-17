9c193a229c026faf134f362c4e5c40c8c92aacc3
maheswari <mahi.sgs22@gmail.com>
2025-07-17 16:06:53 +0530

    docker

--- main.tf
+++ main.tf
@@ -19,14 +19,14 @@ resource "aws_security_group" "allow_all_docker" {
         from_port = 0
         to_port = 0
         protocol = "-1"
-        cidr_block = ["0.0.0.0/0"]
+        cidr_blocks = ["0.0.0.0/0"]
         ipv6_cidr_blocks = ["::/0"]
     }
     egress {
         from_port = 0
         to_port = 0
         protocol = "-1"
-        cidr_block = ["0.0.0.0/0"]
+        cidr_blocks = ["0.0.0.0/0"]
         ipv6_cidr_blocks = ["::/0"]
     }
     lifecycle {
